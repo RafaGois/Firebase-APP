52e85005493b02438ddf4bef9493fb6843faf024
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2022-10-11 15:11:31 -0300

    up

--- app/src/main/java/com/example/firebase_app/MainActivity.java
+++ app/src/main/java/com/example/firebase_app/MainActivity.java
@@ -1,20 +1,50 @@
 package com.example.firebase_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.ktx.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Firebase firebase;
+    private String TAG = "AAA";
+
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DatabaseReference usuarios = reference.child("usuarios");
 
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        /*
+        Usuario usuario = new Usuario();
+        usuario.setNome("Sandra");
+        usuario.setSobrenome("Fontana");
+        usuario.setIdade(48);
+
+
+
+        usuarios.child("002").setValue(usuario);
+         */
     }
 }
